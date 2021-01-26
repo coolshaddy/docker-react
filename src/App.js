6eef23bccd199ce72dc64eaf5f01ebe8d6ce6413
Sandeep Singh Chouhan <coolshaddy81@gmail.com>
2021-01-26 11:44:46 +0530

    changed app js for feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           navdeep or sandeep dono bhai bhai hhh
+           dj wale babu
         </p>
         <a
           className="App-link"
