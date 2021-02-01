b05547713fede1407ff3b509eb2098cfdad1db4d
coolshaddy <coolshaddy81@gmail.com>
2021-02-01 16:55:00 +0530

    new commit rule

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           demo site
+           demo website
         </p>
         <a
           className="App-link"
